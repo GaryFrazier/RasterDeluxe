0c8849e8890435e8984d6562ddee7f249a20313b
GaryFrazier95 <garyfrazier95@yahoo.com>
2024-03-02 13:32:54 -0500

    message box winapi test

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
+#define UNICODE
 #include <stdio.h>
+#include <windows.h>
 
 int main(void) {
 	printf("Hello World!\n");
+
+	LPCSTR title = "Title!";
+	LPCSTR message = "HEY!";
+	MessageBoxA(NULL, message, title, MB_OKCANCEL);	
 	return 0;
 }
