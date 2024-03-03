0460be90b85644e8671b68b0726db08be8346d50
GaryFrazier95 <garyfrazier95@yahoo.com>
2024-03-02 19:39:16 -0500

    window

--- src/main.c
+++ src/main.c
@@ -2,11 +2,42 @@
 #include <stdio.h>
 #include <windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	printf("Message Handled\n");
+}
+
 int main(void) {
 	printf("Hello World!\n");
 
-	LPCSTR title = "Title!";
-	LPCSTR message = "HEY!";
-	MessageBoxA(NULL, message, title, MB_OKCANCEL);	
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+	const LPCSTR windowClassName = "Window Class";
+	WNDCLASS windowClass = {};
+
+	windowClass.lpfWndProc = WindowProc;
+	windowClass.hInstance = hInstance;
+	windowClass.lpszClassName = windowClassName;
+
+	RegisterClass(&windowClass);
+
+	HWND hwnd = CreateWindowEx(
+			0,                              // Optional window styles.
+			windowClassName,                     // Window class
+			L"Learn to Program Windows",    // Window text
+			WS_OVERLAPPEDWINDOW,            // Window style
+
+			// Size and position
+			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+			NULL,       // Parent window    
+			NULL,       // Menu
+			hInstance,  // Instance handle
+			NULL        // Additional application data
+			);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
 	return 0;
 }
